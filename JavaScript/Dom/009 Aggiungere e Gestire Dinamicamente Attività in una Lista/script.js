faf37d314e3de875499c884d1029330da64893b6
TalpaDev <luppialessandro@istitutocorni.it>
2026-03-20 08:59:35 +0100

    Esercizi

--- JavaScript/Dom/009 Aggiungere e Gestire Dinamicamente Attività in una Lista/script.js
+++ JavaScript/Dom/009 Aggiungere e Gestire Dinamicamente Attività in una Lista/script.js
@@ -1,14 +1,58 @@
+var numeroAttivita = 0;
+var numeroAttivitaCompletate = 0;
+
 function aggiungi() {
   // Ottenere valore input
-  const input = document.querySelector("#Input").value;
+  const input = document.querySelector("#Input");
+  const inputValue = input.value;
+
   // Creare elemento li
+  const li = document.createElement("li");
+  li.innerText = inputValue;
+
   // Aggiungere br
+  // const br = document.createElement("br");
+
   // Creare bottone
+  const button = document.createElement("button");
+  button.innerText = "Attività Completata";
+
   // Implementare onclick del bottone
+  button.addEventListener("click", () => {
+    if (li.style.textDecoration === "line-through") {
+      li.style.textDecoration = "none";
+      button.innerText = "Attività Completata";
+      numeroAttivitaCompletate--;
+    } else {
+      li.style.textDecoration = "line-through";
+      button.innerText = "Segna incompleta";
+      numeroAttivitaCompletate++;
+    }
+  });
+
   // Aggiungere bottone al li
+  li.appendChild(button);
+
   // Aggiungere li alla lista
+  const lista = document.querySelector("#elenco");
+  lista.appendChild(li);
+  numeroAttivita++;
+
   // Pulire input
+  input.value = "";
+
   // Aggiornare totale
+  document.querySelector("#totaleAttivita").innerText = numeroAttivita;
+  document.querySelector("#attivitaCompletate").innerText =
+    numeroAttivitaCompletate;
 }
 
-function riepilogo() {}
+function riepilogo() {
+  const elemento = document.querySelector("#nascosto");
+  elemento.style.display = "block";
+}
+
+function reset() {
+  const lista = document.querySelector("#elenco");
+  lista.innerHTML = "";
+}
---
+++ JavaScript/Dom/010 Gestione prodotti magazzino/script.js
@@ -0,0 +1,17 @@
+const tabellaOriginale = document.querySelector("#tab");
+
+function eliminaPerCodice() {
+  const elementi = document.querySelectorAll("#tab tr")
+}
+
+function registraVendita() {}
+
+function filtraProdotti() {}
+
+function ripristinaTabella() {
+  document.querySelector("#tab").innerHTML = tabellaOriginale;
+}
+
+function ordinaPerNome() {}
+
+function aggiornaContatore() {}
